7d7fc20c423eaac2bd343d08e21856fe44b6c6e5
BorealAce <gabrielborgesgs20@gmail.com>
2025-03-03 20:52:02 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao jogo do numero secreto!');
+let numeroSecreto = 9;
+let chute = prompt('Insira um numero entre 1 e 10');
+
+if (chute == numeroSecreto) {
+    console.log('Isso ai! Voce acertou o numero secreto (9).');
+}
